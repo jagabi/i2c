9f83c71c5b7fc8f00b38ea3e4992fb4386dc9766
kys <crosstosaw@naver.com>
2023-02-06 15:24:04 +0900

    i2c

---
+++ backup/backup.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+
+int currentData = 0;
+char *sentence = "HELLO";
+float actual_num = 1.2;
+const int Address = 0x50;
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin(Address);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(sendDouble);
+}
+
+void receiveEvent(int bytes) {
+  int data = Wire.read();
+  Serial.print("Receive Data: ");
+  Serial.println(data);
+}
+
+void loop()
+{
+  delay(100);
+  //currentData += 1;
+}
+
+void sendInt(){
+      Wire.write(currentData);
+}
+
+void sendStr(){
+    for(int i=0; i<strlen(sentence);i++)
+    {
+      Wire.write(sentence[i]);
+    }
+}
+
+void sendDouble(){
+  String temp = String(actual_num,1);
+  Serial.println(temp);
+  for(int i=0; i<temp.length();i++)
+  {
+     Wire.write(temp[i]);
+  }
+}
---
+++ backup/backup_i2c.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import smbus as sm
+
+address = 0x50
+bus_num = 1
+bus = sm.SMBus(bus_num)
+
+def writeNumber(value):
+    bus.write_byte(address, value)
+    return
+
+def readNumber():
+    number = bus.read_byte(address)
+    return number
+
+def readDouble():
+    str_num = ""
+    str_num_list = bus.read_i2c_block_data(address,0)
+    for i in str_num_list:
+        if(i == 255):
+            break
+        else:
+            str_num += chr(i)
+    actual_num = float(str_num) 
+    return actual_num
+    
+     
+
+def readString():
+    sentence = ""
+    asc = bus.read_i2c_block_data(address,0)
+    for i in asc:
+        if(i == 255):
+            break
+        else:
+            sentence += chr(i)
+    return sentence
+    
+while True:
+    data = 12
+    writeNumber(data)
+    num = readDouble()
+    #sen = readString()
+
+    print("read data:", num)
